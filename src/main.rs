a5632effd73cffdd3da9b96d0f40cf7c746e9a65
Andre Beckedorf <andre.beckedorf@gmail.com>
2023-04-18 22:12:05 +0200

    Krauli Krauli

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::{fs, time::UNIX_EPOCH, os::unix::prelude::MetadataExt};
+use std::{fs};
 
-use chrono::{Date, TimeZone, NaiveDate, DateTime, Utc};
+use chrono::{NaiveDate, DateTime, Utc};
 use clap::{Arg, Command};
 use walkdir::WalkDir;
 
@@ -40,16 +40,36 @@ fn parse_files(input_files : &Vec<String>) -> anyhow::Result<Vec<FileEntry>> {
     Ok(result)
 }
 
+fn move_files(parse_files: &Vec<FileEntry>, target_root: &str) -> anyhow::Result<()> {
+
+    let mut target_dirs = Vec::new();
+    for entry in WalkDir::new(target_root) {
+        let entry = entry?;
+        if !entry.metadata()?.is_dir() {
+            continue;
+        }
+
+        target_dirs.push(entry.path().display().to_string());
+    }
+
+    println!("target_root: {:?}", target_root);
+    println!("target_dirs: {:?}", target_dirs);
+
+    Ok(())
+}
+
 fn main() -> anyhow::Result<()> {
     let matches = Command::new("photosort")
         .arg(Arg::new("input").required(true))
+        .arg(Arg::new("output").required(true))
         .get_matches();
 
     let input_path: &String = matches.get_one::<String>("input").unwrap();
     let input_files = list_files(input_path, Some(".JPG"))?;
-
     let parsed_files = parse_files(&input_files)?;
 
-    println!("Files: {:?}", parsed_files);
+    let output_path: &String = matches.get_one::<String>("output").unwrap();
+    move_files(&parsed_files, output_path)?;
+
     Ok(())
 }
